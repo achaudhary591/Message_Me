9f4f1bec39b47a7eec22f79f4c1e6042fc742f59
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-12-06 17:30:11 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(username: "Anurag", password: "password")
+# User.create(username: "Pratibha", password: "password")
+# User.create(username: "Asad", password: "password")
+# User.create(username: "Ajeet", password: "password")
+# User.create(username: "Vipul", password: "password")
+# User.create(username: "Adnan", password: "password")
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Jonsnow", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
