7b62f5d91745aa37e51c13ef09f886e8f0145b16
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-12-07 14:40:07 +0530

    added restriction for no logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
   end
 
   def create
-
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
@@ -20,6 +20,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
